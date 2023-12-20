b6f4d5e2447ab8be9306bb6de136b6e72b76ea81
Shobhit Behl <shobhitbehl98@gmail.com>
2023-12-20 19:54:14 +0530

    github link

--- mernapp/src/components/Footer.js
+++ mernapp/src/components/Footer.js
@@ -6,9 +6,9 @@ export default function Footer() {
   return (
     <div><footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div className="col-md-4 d-flex align-items-center">
-      <Link to="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+      <span style={{marginRight:'1rem',marginLeft:'1rem'}} className="text-muted">© { new Date().getFullYear() } {process.env.REACT_APP_TITLE}, Inc</span>
+      <Link to="https://github.com/shobhitbehl98/Zayka" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">Github
       </Link>
-      <span className="text-muted">© { new Date().getFullYear() } {process.env.REACT_APP_TITLE}, Inc</span>
     </div>
 
     <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
