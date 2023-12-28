58d25a008b3728b8df8072cef92ffd6521442950
Shobhit Behl <shobhitbehl98@gmail.com>
2023-12-28 06:54:12 +0530

    db removed global

--- mernapp/backend/Routes/FoodData.js
+++ mernapp/backend/Routes/FoodData.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Food = require('../models/Food')
+const Category = require('../models/Category')
 
 router.post('/foodData',async(req,res)=>{
     try {
-       res.send([global.food_items,global.food_category])
+       const food = await Food.find({});
+       const categories = await Category.find({});
+       console.log(!!food,!!categories);
+       res.send([food,categories])
     } catch (error) {
         console.error(error);
         res.send('Server Error')
