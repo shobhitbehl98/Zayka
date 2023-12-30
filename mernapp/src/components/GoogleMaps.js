705f0bbb59180fc83fd9e187dd730c3eec6c3749
Shobhit Behl <shobhitbehl98@gmail.com>
2023-12-30 16:04:28 +0530

    location change FE

--- mernapp/src/components/GoogleMaps.js
+++ mernapp/src/components/GoogleMaps.js
@@ -29,19 +29,12 @@ function GoogleMaps({ isVisible, closeMaps }) {
       };
     return (
         <div>
-            {isVisible && <div style={{ position: 'fixed', justifyContent: 'center', left: '5%', top: '5%', width: '90vw', height: '90vh', zIndex: 100, backgroundColor: 'white' }}>
-                <div style={{ display: 'flex', justifyContent: 'space-around', color: 'black', height: '10%' }}>
-                    <div style={{ width: '80%', display: 'flex', alignSelf: 'center', justifyContent: 'center' }}>
-
+            {isVisible && <div style={{ position: 'fixed',padding:'1rem', justifyContent: 'center', left: '5%', top: '5%', width: 'fit-content', height: 'fit-content', zIndex: 100, backgroundColor: 'red' }}>
+                    <div style={{color:'black',marginBottom:'1rem'}}>Enter your location:</div>
                         <div classname="input-group">
                             <input type="text" value={inputValue}  onChange={handleInputChange} style={{width:'40vw'}} classname="form-control rounded me-3" placeholder="Enter Value" aria-label="Text" />
                             <button type="button" onClick={postData} classname="btn btn-outline-primary" data-mdb-ripple-init>Select</button>
                         </div>
-
-                    </div>
-                    <div className='fs-2' style={{ width: '5%', cursor: 'pointer' }} onClick={closeMaps}>X</div>
-                </div><div id='container' style={{ position: 'relative', height: '90%', width: '100%', backgroundColor: 'navajowhite', padding: '1rem', border: '0.5rem', borderColor: 'gray' }}></div>
-
             </div>}
         </div>
     )
